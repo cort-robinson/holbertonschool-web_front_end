8ee6424942f8457a42d552ed7125be83ffe40dbc
Cort Robinson <cortrobinson99@gmail.com>
2021-06-22 04:07:56 -0500

    add task 9 file

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,20 @@
+function countPrimeNumbers() {
+    let primeCount = 0;
+
+    const isPrime = n => {
+        for (let i = 2, s = Math.sqrt(n); i <= s; i++)
+            if (n % i === 0) return 0;
+        return 1;
+    };
+
+    for (let i = 2; i < 101; i++)
+        primeCount += isPrime(i);
+
+    return primeCount;
+}
+
+let start = performance.now();
+countPrimeNumbers();
+let end = performance.now();
+let timeElapsed = (end - start);
+console.log('Execution time of printing countPrimeNumbers was ' + timeElapsed + ' milliseconds.');
