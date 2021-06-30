1e71ae2ff4c72a7457743d34342ba76e83573fbb
Cort Robinson <cortrobinson99@gmail.com>
2021-06-30 00:26:09 -0500

    add task 1 files

--- 0x0E-Cookies_local_storage/src/index.js
+++ 0x0E-Cookies_local_storage/src/index.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     function setCookies() {
+        const d = new Date();
+        d.setTime(d.getTime() + (10*24*60*60*1000));
         document.cookie = 'firstname=' + document.getElementById('firstname').value;
         document.cookie = 'email=' + document.getElementById('email').value;
+        document.cookie = 'expires=' + d.toUTCString();
     }
 
     function showCookies() {
